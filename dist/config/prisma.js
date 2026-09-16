284fb7238bee62cc92d72cf4ec669f9489becc13
BrahianCarrera <brahian.carrera@udea.edu.co>
2026-09-16 09:22:17 -0500

    fix: corregir importacion de prisma client para build de produccion

--- dist/config/prisma.js
+++ dist/config/prisma.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
 const adapter_pg_1 = require("@prisma/adapter-pg");
-const client_1 = require("../generated/prisma/client");
+const client_1 = require("@prisma/client");
 const env_js_1 = require("./env.js");
 const globalForPrisma = globalThis;
 function makePrismaClient() {
